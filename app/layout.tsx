fa21adf2aa40b720fc18939356412a6f8e0cc417
mateivic <mateivic3007@gmail.com>
2025-12-10 12:57:44 +0100

    title tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -239,6 +239,7 @@ export default function RootLayout({
   return (
     <html lang="hr">
       <head>
+        <title>DigitalMix - Vođenje Društvenih Mreža i Kreiranje Sadržaja</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossOrigin=""/>
